2621da4e92dea1cb6cbb826c87b007af2dd797e9
Boris <boris021@mail.ru>
2022-05-06 11:11:56 +0500

    work

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -6,3 +6,11 @@ import {
 import {
 	flsModules
 } from "./modules.js";
+
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
